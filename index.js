864fda322309b541d5acbc889973b79316ac9f67
Pascal de Vink <pascal@pascaldevink.nl>
2023-03-29 10:51:11 +0100

    Add binary executable

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const { program } = require('commander');
 const fs = require('fs');
 const ptify = require('pseudo-translate');
 
-program
-  .name('pseudo-translate-cli')
-  .description('CLI to translate a JSON file to a pseudo language with expanded characters')
-  .requiredOption('-i, --input <input>')
-  .option('-e, --expand');
+function init() {
+  program
+    .name('pseudo-translate-cli')
+    .description('CLI to translate a JSON file to a pseudo language with expanded characters')
+    .requiredOption('-i, --input <input>')
+    .option('-e, --expand');
 
-program.parse();
+  program.parse();
 
-const options = program.opts();
+  const options = program.opts();
 
-const pt = ptify(options.input, { increase_lengh_30_pct: options.expand });
-console.log(JSON.stringify(pt, null, " ")); 
+  const pt = ptify(options.input, { increase_lengh_30_pct: options.expand });
+  console.log(JSON.stringify(pt, null, " ")); 
+}
 
+module.exports = {
+  init
+};
